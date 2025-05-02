4207f13004356ce6ff35db1f1bf7450cef1ba399
GCCI <154489990+gccci@users.noreply.github.com>
2025-05-02 10:19:07 +0500

    Update

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("/Users/gcci_web3/Documents/GitHub/docusaurustest/node_modules/infima/dist/css/default/default.css"),
+  require("/Users/gcci_web3/Documents/GitHub/docusaurustest/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Users/gcci_web3/Documents/GitHub/docusaurustest/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/Users/gcci_web3/Documents/GitHub/docusaurustest/src/css/custom.css"),
+];
